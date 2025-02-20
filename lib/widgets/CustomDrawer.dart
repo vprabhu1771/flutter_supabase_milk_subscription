19cfbce5e52c2e4f29211104015e2aa09380d3b0
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-20 16:07:54 +0530

    ProfileScreen added to CustomDrawer

--- lib/widgets/CustomDrawer.dart
+++ lib/widgets/CustomDrawer.dart
@@ -6,6 +6,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../screens/HomeScreen.dart';
 import '../screens/auth/LoginScreen.dart';
+import '../screens/auth/ProfileScreen.dart';
 import '../screens/auth/RegisterScreen.dart';
 
 final supabase = Supabase.instance.client;
@@ -60,6 +61,19 @@ class CustomDrawer extends StatelessWidget {
               onTap: () {
                 Navigator.pop(context);
                 Navigator.pushNamed(context, '/settings');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.contacts),
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  parentContext,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(title: 'Profile'),
+                  ),
+                );
               },
             ),
             Divider(),
