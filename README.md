8d7275b61f95bae6621923c5717e7ae081c6310c
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-20 15:55:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_supabase_milk_subscription
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_supabase_milk_subscription/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_supabase_milk_subscription
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
