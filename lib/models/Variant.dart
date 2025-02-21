e358388f676c95a4e6252feff4bf290b204f4d5b
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-21 08:58:36 +0530

    Create Variant.dart

---
+++ lib/models/Variant.dart
@@ -0,0 +1,31 @@
+class Variant {
+  final int productId;
+  final int id;
+  final String qty;
+  final String unitPrice;
+
+  Variant({
+    required this.productId,
+    required this.id,
+    required this.qty,
+    required this.unitPrice,
+  });
+
+  factory Variant.fromJson(Map<String, dynamic> json) {
+    return Variant(
+      productId: json['product_id'],
+      id: json['id'],
+      qty: json['qty'],
+      unitPrice: json['unit_price'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'product_id': productId,
+      'id': id,
+      'qty': qty,
+      'unit_price': unitPrice,
+    };
+  }
+}
