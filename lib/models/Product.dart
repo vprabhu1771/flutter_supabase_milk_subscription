e245f8cbd8d2ec58becf05153050550d1a4ecef6
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-21 08:58:40 +0530

    Create Product.dart

---
+++ lib/models/Product.dart
@@ -0,0 +1,37 @@
+import 'Variant.dart';
+
+class Product {
+  final int id;
+  final String name;
+  final String image_path;
+  final List<Variant> variants;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.image_path,
+    required this.variants,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    var variantsJson = json['variants'] as List;
+    List<Variant> variantList =
+    variantsJson.map((variant) => Variant.fromJson(variant)).toList();
+
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      image_path: json['image_path'],
+      variants: variantList,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'image_path': image_path,
+      'variants': variants.map((variant) => variant.toJson()).toList(),
+    };
+  }
+}
