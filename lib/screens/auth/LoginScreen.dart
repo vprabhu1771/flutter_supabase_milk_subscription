125ac55da8543e0573ce92a1b48c7d0c1a9260da
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-28 11:39:01 +0530

    LoginScreen Role Dropdown added

--- lib/screens/auth/LoginScreen.dart
+++ lib/screens/auth/LoginScreen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_supabase_milk_subscription/delivery/DeliveryDashboard.dart';
 import 'package:flutter_supabase_milk_subscription/screens/HomePage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../../admin/AdminDashboard.dart';
 import '../HomeScreen.dart';
 import 'RegisterScreen.dart';
 
@@ -23,16 +25,58 @@ class _LoginScreenState extends State<LoginScreen> {
 
   final TextEditingController passwordController = TextEditingController();
 
+  String _selectedRole = 'Customer';
+
+  final List<String> _roles = ['Customer', 'Delivery', 'Admin'];
+
   final storage = FlutterSecureStorage(); // Secure storage instance
 
   bool _isPasswordVisible = false; // Track password visibility
 
+  String? userId;
+
   @override
   void initState() {
     super.initState();
-    emailController.text = "admin@gmail.com";
+    emailController.text = "prabhu@gmail.com";
     passwordController.text = "admin@123";
   }
+
+  Future<void> fetchUserRole(String userId) async {
+    try {
+      final response = await Supabase.instance.client
+          .from('user_roles')
+          .select('roles(id, name)')
+          // .select('*')
+          .eq('user_id', userId)
+          .maybeSingle(); // Avoids crash if no rows are found
+
+      print(response.toString());
+
+      if (response != null && response['roles'] != null) {
+        final role = response['roles']['name'];
+        print("Role: $role");
+        navigateBasedOnRole(role);
+      } else {
+        print("No role found for user: $userId");
+      }
+    } catch (e) {
+      print("Error fetching role: $e");
+    }
+  }
+
+
+
+  void navigateBasedOnRole(String role) {
+    if (role == 'admin') {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => AdminDashboard(title: "Admin Dashboard",)));
+    } else if (role == 'customer') {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen(title: 'Home')));
+    } else if (role == 'delivery') {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => DeliveryDashboard(title: "Delivery Dashboard",)));
+    }
+  }
+
 
   Future<void> signIn() async {
     try {
@@ -49,11 +93,19 @@ class _LoginScreenState extends State<LoginScreen> {
           SnackBar(content: Text('Login successful!')),
         );
 
+        // print(supabase.auth.currentUser?.id);
+        setState(() {
+          userId = supabase.auth.currentUser?.id;
+        });
+
+        print(userId);
+
         // Navigate to home screen
-        Navigator.pop(context);
-        Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => HomePage()),
-        );
+        // Navigator.pop(context);
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(builder: (context) => HomePage()),
+        // );
+        fetchUserRole(userId!);
       }
     } catch (error) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -92,6 +144,30 @@ class _LoginScreenState extends State<LoginScreen> {
               obscureText: !_isPasswordVisible,
             ),
             SizedBox(height: 20),
+
+            // Role Dropdown
+            DropdownButtonFormField<String>(
+              value: _selectedRole,
+              items: _roles.map((role) {
+                return DropdownMenuItem(
+                  value: role,
+                  child: Text(role),
+                );
+              }).toList(),
+              decoration: InputDecoration(
+                labelText: "Select Role",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _selectedRole = value!;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+
             ElevatedButton(
               onPressed: signIn,
               child: Text('Login'),
